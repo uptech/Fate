ad1620edda9023c5ca4158c279f2822364f5b326
Andrew De Ponte <cyphactor@gmail.com>
2021-01-15 20:43:51 -0500

    Remove transformed() from Fate.Future
    I did this because it is technically a duplicate of Fate.Future.mapValue
    which I think is a more natural name for what is going on.
    
    [changelog]
    removed: Fate.Future.transformed(closure:) from the API
    
    ps-id: 5A0FE532-32C2-470E-B264-E50E454FDD2C

--- Sources/Fate/Transformed.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-extension Fate.Future {
-    public func transformed<NextValue, EM: Error>(with closure: @escaping (V) throws -> NextValue) -> Future<NextValue, EM> {
-        return chained { value in
-            return try Promise(value: closure(value))
-        }
-    }
-}

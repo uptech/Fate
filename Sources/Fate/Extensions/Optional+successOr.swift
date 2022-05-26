93676e71c1560a76b8bdd9186f3224c762c9c393
Philip Johnson <philip@uptechstudio.com>
2022-05-26 08:24:58 -0700

    Add Optional successOr extension
    This extension function converts an Optional into a future.
    
    ps-id: 7796ab15-5a94-4b97-8e01-313b615d13e2

---
+++ Sources/Fate/Extensions/Optional+successOr.swift
@@ -0,0 +1,11 @@
+import Foundation
+
+extension Optional {
+    func successOr<E: Error>(_ error: E) -> Future<Wrapped, E> {
+        if let v = self {
+            return Promise<Wrapped, E>(value: v)
+        } else {
+            return Promise<Wrapped, E>(error: error)
+        }
+    }
+}
